e224178bf031d3e9af0d1fddb2cb5973ab6b3c47
Yuriy Vinogradov <discours@list.ru>
2014-01-21 11:24:59 +0400

    Interface refactoring
    and some minor fix

--- wordsLearner/wordsLearner/src/main/java/com/example/wordslearner/NameWordSetDialog.java
+++ wordsLearner/wordsLearner/src/main/java/com/example/wordslearner/NameWordSetDialog.java
@@ -1,18 +1,36 @@
 package com.example.wordslearner;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Юрий on 19.01.14.
  */
 
-public class NameWordSetDialog extends Activity {
+public class NameWordSetDialog extends Activity implements View.OnClickListener {
+
+    Button btnSave;
+    EditText wordSetTitleET;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.namewordsetdialog);
+
+        btnSave = (Button) findViewById(R.id.btnSaveTitle);
+        wordSetTitleET = (EditText) findViewById(R.id.wordSetTitleET);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch(view.getId()){
+            case R.id.btnSaveTitle :
+                break;
+        }
     }
 }
