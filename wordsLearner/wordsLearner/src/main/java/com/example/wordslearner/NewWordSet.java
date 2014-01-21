3a83af3522e334cecdb6d0838b8e6ddbeaa46cd7
Yuriy Vinogradov <discours@list.ru>
2014-01-21 16:09:21 +0400

    db coding
    commit with errors

--- wordsLearner/wordsLearner/src/main/java/com/example/wordslearner/NewWordSet.java
+++ wordsLearner/wordsLearner/src/main/java/com/example/wordslearner/NewWordSet.java
@@ -1,7 +1,11 @@
 package com.example.wordslearner;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -22,6 +26,8 @@ public class NewWordSet extends Activity implements OnClickListener {
     Button btnSaveWord, btnSaveWordSet;
     Intent intent;
     Map<String, String> wordSet;
+    DbHelper dbHelper;
+    String wordSetTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +47,8 @@ public class NewWordSet extends Activity implements OnClickListener {
         btnSaveWordSet.setOnClickListener(this);
 
         wordSet = new HashMap<String, String>();
+
+        dbHelper = new DbHelper(this);
     }
 
     @Override
@@ -48,18 +56,64 @@ public class NewWordSet extends Activity implements OnClickListener {
         if (data == null) {
             return;
         }
-        String wordSetTitle = data.getStringExtra("wordSetTitle");
+        wordSetTitle = data.getStringExtra("wordSetTitle");
         titleTW.setText(titleTW.getText() + " " + wordSetTitle);
     }
 
     @Override
     public void onClick(View view) {
+
+
         switch (view.getId()) {
-            case R.id.btnSaveWord :
+            case R.id.btnSaveWord:
                 wordSet.put(foreignET.getText().toString(), translationET.getText().toString());
                 break;
-            case R.id.btnSaveWordSet :
+            case R.id.btnSaveWordSet:
+                SQLiteDatabase db = dbHelper.getWritableDatabase();
+                ContentValues cv = new ContentValues();
+                cv.put("title", wordSetTitle);
+                long wordSetId = db.insert("WordSets", null, cv);
+
+                for (Map.Entry<String, String> entry : wordSet.entrySet()) {
+                    cv = new ContentValues();
+                    cv.put("foreign", entry.getKey());
+                    cv.put("translation", entry.getValue());
+                    cv.put("wordSetId", wordSetId);
+                    db.insert("Words", null, cv);
+                }
                 break;
+        }
+        dbHelper.close();
+    }
+
+    class DbHelper extends SQLiteOpenHelper {
+
+        public DbHelper(Context context) {
+            super(context, "myDB", null, 1);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            StringBuilder sb = new StringBuilder();
+            sb.append("CREATE TABLE WordSets(");
+            sb.append("id integer primary key autoincrement, ");
+            sb.append("title text");
+            sb.append(");");
+
+            db.execSQL(sb.toString());
+
+            sb = new StringBuilder();
+            sb.append("CREATE TABLE Words(");
+            sb.append("id integer primary key autoincrement, ");
+            sb.append("foreign text, ");
+            sb.append("translation text, ");
+            sb.append("wordSetId integer");
+            sb.append(");");
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i2) {
+
         }
     }
 }
