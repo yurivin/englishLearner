f6676b234ff1fc9b8f200d3b5b13f870e4e7b66c
Iury V. Vinogradov <discours@list.ru>
2014-04-02 08:54:49 +0400

    download words in progress ui

--- wordsLearner/wordsLearner/src/main/java/net/yuvin/dictionarisk/view/WordsToDownload.java
+++ wordsLearner/wordsLearner/src/main/java/net/yuvin/dictionarisk/view/WordsToDownload.java
@@ -14,20 +14,16 @@ import net.yuvin.dictionarisk.model.WordSetProperties;
 public class WordsToDownload extends BaseActivity implements View.OnClickListener {
 
     private TextView twTitle;
-    private Button btnAddWord, btnSend;
+    private Button btnAddWord, btnSend, dtnSave;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.editwordsset);
+        setContentView(R.layout.wordstodownload);
 
-        twTitle = (TextView) findViewById(R.id.editWordsSetTitleTV);
-        twTitle.setText(getString(R.string.defineWordsToDownloadTranslations));
-
+        twTitle = (TextView) findViewById(R.id.tvDownloadWordsTitle);
         btnAddWord = (Button)findViewById(R.id.btnNewWord);
-        btnAddWord.setText(R.string.add_word);
-
-        btnSend = (Button)findViewById(R.id.btnRenameSet);
-        btnSend.setText(R.string.send);
+        btnSend = (Button)findViewById(R.id.btnSend);
+        dtnSave = (Button)findViewById(R.id.btnSave);
 
         WordSetProperties wordSetProperties = new WordSetProperties();
         wordSetProperties.setLanguageFrom(getIntent().getStringExtra("LanguageFrom"));
